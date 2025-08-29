7f86cb15d965b72cf445f34850df2ce81c7ed90a
ksharp <kelly@letsgomint.com>
2025-08-29 14:24:11 -0700

    jkl

--- backend/dist/config/plugins.js
+++ backend/dist/config/plugins.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
+    superfields: {
+        enabled: true,
+    },
     documentation: {
         enabled: true,
         config: {
